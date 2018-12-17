f370c02481e1d8f25a1612e046770c878617ca78
benny <35829331+yanx27@users.noreply.github.com>
2018-12-17 20:33:59 +0800

    step1

--- make_source.py
+++ make_source.py
@@ -57,43 +57,16 @@ model = torch.nn.DataParallel(model).cuda()
 model.float()
 model.eval()
 
-'''check'''
-img_path = sorted(img_dir.iterdir())[137]
-import os
-path = os.getcwd()
-img = cv2.imread('./data/source/images/img_999.png')
-shape_dst = np.min(img.shape[:2])
-# offset
-oh = (img.shape[0] - shape_dst) // 2
-ow = (img.shape[1] - shape_dst) // 2
-
-img = img[oh:oh + shape_dst, ow:ow + shape_dst]
-img = cv2.resize(img, (512, 512))
-
-plt.imshow(img[:, :, [2, 1, 0]])  # BGR -> RGB
-
-
-multiplier = get_multiplier(img)
-with torch.no_grad():
-    paf, heatmap = get_outputs(multiplier, img, model, 'rtpose')
-
-r_heatmap = np.array([remove_noise(ht)
-                      for ht in heatmap.transpose(2, 0, 1)[:-1]]) \
-    .transpose(1, 2, 0)
-heatmap[:, :, :-1] = r_heatmap
-param = {'thre1': 0.1, 'thre2': 0.05, 'thre3': 0.5}
-label = get_pose(param, heatmap, paf)
-
-plt.imshow(label)
-plt.show()
-
 '''make label images for pix2pix'''
 test_img_dir = save_dir.joinpath('test_img')
 test_img_dir.mkdir(exist_ok=True)
 test_label_dir = save_dir.joinpath('test_label_ori')
 test_label_dir.mkdir(exist_ok=True)
+test_head_dir = save_dir.joinpath('test_head_ori')
+test_head_dir.mkdir(exist_ok=True)
 
-for idx in tqdm(range(200,400)):
+pose_cords = []
+for idx in tqdm(range(len(os.listdir(str(img_dir))))):
     img_path = img_dir.joinpath('{:05}.png'.format(idx))
     img = cv2.imread(str(img_path))
     shape_dst = np.min(img.shape[:2])
@@ -110,8 +83,26 @@ for idx in tqdm(range(200,400)):
         .transpose(1, 2, 0)
     heatmap[:, :, :-1] = r_heatmap
     param = {'thre1': 0.1, 'thre2': 0.05, 'thre3': 0.5}
-    label,_ = get_pose(param, heatmap, paf)
+    label, cord = get_pose(param, heatmap, paf)
+    index = 13
+    crop_size = 25
+    try:
+        head_cord = cord[index]
+    except:
+        head_cord = pose_cords[-1] # if there is not head point in picture, use last frame
+
+    pose_cords.append(head_cord)
+    head = img[int(head_cord[1] - crop_size): int(head_cord[1] + crop_size),
+           int(head_cord[0] - crop_size): int(head_cord[0] + crop_size), :]
+    plt.imshow(head)
+    plt.savefig(str(test_head_dir.joinpath('pose_{}.jpg'.format(idx))))
+
     cv2.imwrite(str(test_img_dir.joinpath('{:05}.png'.format(idx))), img)
     cv2.imwrite(str(test_label_dir.joinpath('{:05}.png'.format(idx))), label)
+    if idx % 100 == 0 and idx != 0:
+        pose_cords_arr = np.array(pose_cords, dtype=np.int)
+        np.save(str((save_dir.joinpath('pose_yx.npy'))), pose_cords_arr)
 
+pose_cords_arr = np.array(pose_cords, dtype=np.int)
+np.save(str((save_dir.joinpath('pose_yx.npy'))), pose_cords_arr)
 torch.cuda.empty_cache()
